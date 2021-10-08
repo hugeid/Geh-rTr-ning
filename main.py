3d9e021e36df5a88e19d9c71ddfdabaf053acd26
ziepziep <hugo.eidmann@hotmail.com>
2021-10-08 10:44:54 +0200

    asdkasl;dka

--- main.py
+++ main.py
@@ -11,3 +11,18 @@ note = random.choice(notes)
 print(f"Playing note {note}")
 playsound(f"{cwd}/soundbank/{note}")
 
+
+def main():
+    pass
+
+def play_sound(sounds, note):
+    pass
+
+def play_random_note(sounds):
+    pass
+
+def menu(desc, prompt, options):
+    pass
+
+def guess(guess, answer):
+    return guess == answer
